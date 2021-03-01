d614fb98add14aaafa4a42f687ff98fe693ab0f1
Techpc23 <mh2624127@gmail.com>
2021-03-01 17:13:31 +0600

    This is sample Bottom Navigation view

---
+++ app/src/main/java/com/example/bottomnavview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bottomnavview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        navigationView.setSelectedItemId(R.id.home);
+
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(MainActivity.this,homeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+
+                return false;
+            }
+        });
+    }
+}
